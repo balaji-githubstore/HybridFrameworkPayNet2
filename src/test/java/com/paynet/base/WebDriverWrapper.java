37889bf11943084fbdef6940588b9e58f2f49ff8
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-03-31 09:49:58 +0530

    cross browser testing

--- src/test/java/com/paynet/base/WebDriverWrapper.java
+++ src/test/java/com/paynet/base/WebDriverWrapper.java
@@ -4,6 +4,8 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -15,9 +17,22 @@ public class WebDriverWrapper {
 
 	@BeforeMethod
 	public void setup() {
-		WebDriverManager.chromedriver().setup();
 
-		driver = new ChromeDriver();
+		String browserName = "ch";
+
+		if (browserName.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver=new EdgeDriver();
+
+		} else if (browserName.equalsIgnoreCase("ff")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+
+		} else {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
