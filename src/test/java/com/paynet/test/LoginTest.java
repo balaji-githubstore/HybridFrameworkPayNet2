57c41fc4da972e6e1949712a09ab00143ef333e0
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-03-31 07:55:42 +0530

    dataprovider for valid credential

--- src/test/java/com/paynet/test/LoginTest.java
+++ src/test/java/com/paynet/test/LoginTest.java
@@ -5,54 +5,28 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import com.paynet.base.WebDriverWrapper;	
+import com.paynet.base.WebDriverWrapper;
+import com.paynet.utilities.DataUtils;
 
 public class LoginTest extends WebDriverWrapper {
-	
-	@Test
-	public void validCredentialTest()
-	{
-		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
-		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
-		driver.findElement(By.name("Submit")).click();
-		
-		Assert.assertEquals(driver.getCurrentUrl(), 
-				"https://opensource-demo.orangehrmlive.com/index.php/dashboard");
-	}
-	
-	
-	//create a dataprovider - invalidCredentialData()
-	
-	//(john,john123,Invalid credentials)
-	//(mark,mark123,Invalid credentials)
-	
-	@DataProvider
-	public String[][] invalidCredentialData()
-	{
-		String[][] main=new String[2][3];
-		
-		main[0][0]="john";
-		main[0][1]="john123";
-		main[0][2]="Invalid credentials";
-		
-		main[1][0]="mark";
-		main[1][1]="mark123";
-		main[1][2]="Invalid credentials";
-		
-		return main;
-	}
-	
-	
-	
-	@Test(dataProvider = "invalidCredentialData")
-	public void invalidCredentialTest(String username,String password,String expectedError)
-	{
+
+	@Test(dataProviderClass = DataUtils.class,dataProvider = "validCredentialData")
+	public void validCredentialTest(String username, String password, String expectedUrl) {
 		driver.findElement(By.id("txtUsername")).sendKeys(username);
 		driver.findElement(By.id("txtPassword")).sendKeys(password);
 		driver.findElement(By.name("Submit")).click();
-		
-		String actualError=driver.findElement(By.id("spanMessage")).getText();
-		
+
+		Assert.assertEquals(driver.getCurrentUrl(), expectedUrl);
+	}
+
+	@Test(dataProviderClass = DataUtils.class, dataProvider = "invalidCredentialData")
+	public void invalidCredentialTest(String username, String password, String expectedError) {
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.name("Submit")).click();
+
+		String actualError = driver.findElement(By.id("spanMessage")).getText();
+
 		Assert.assertEquals(actualError, expectedError);
 	}
 }
